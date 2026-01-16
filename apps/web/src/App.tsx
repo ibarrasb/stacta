c66aef43b4d0bf69187ad4ae8c8686389d1c499b
Eddie Ibarra <eddieibarra43@gmail.com>
2026-01-15 23:42:10 -0600

    init web app (Vite + React TS + Tailwind v4 + shadcn)

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <Button>Stacta</Button>
+    </div>
+  );
+}
